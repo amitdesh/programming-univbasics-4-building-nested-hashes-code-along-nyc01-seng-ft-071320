5ef45e013f8256a7e0d655ada47f6f0df531f588
A D <amitdesh2014@gmail.com>
2020-06-25 01:51:17 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,9 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriach => {}, :matriarch => {}, :hero => {}, :hero_friends => []
+      :patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
       }, 
     :capulet => {
-      :patriach => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []
+      :patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []
       }}
 end
